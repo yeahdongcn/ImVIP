84f0fdc059060eeafdbc257746dcd35fc1e31a77
R0CKSTAR <yeahdongcn@gmail.com>
2014-02-20 16:27:09 +0800

    Add font to display card code and update card layout

--- ImVIP/ImVIP/RSCardView.m
+++ ImVIP/ImVIP/RSCardView.m
@@ -8,6 +8,29 @@
 
 #import "RSCardView.h"
 
+@interface RSCardView ()
+
+@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+
+@property (nonatomic, weak) IBOutlet UILabel *codeLabel;
+
+@property (nonatomic, weak) IBOutlet UIImageView *iconView;
+
+@property (nonatomic, weak) IBOutlet UILabel *usernameLabel;
+
+@end
+
 @implementation RSCardView
 
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.titleLabel.font = [UIFont fontWithName:@"FZQingKeBenYueSongS-R-GB" size:18];
+            self.codeLabel.font = [UIFont fontWithName:@"Farrington-7B-Qiqi" size:16];
+        });
+    }
+    return self;
+}
 @end
